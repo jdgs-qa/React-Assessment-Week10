615ce1267bcd40abe19d89df8b105ce51a5aa537
jdgs-qa <jonathan.squire@academytrainee.com>
2019-08-16 11:11:36 +0100

    Float Right & respond to disruptions

--- src/StatusBlock.js
+++ src/StatusBlock.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 function StatusBlock(props) {
-
-    return (
-        <div className="card m-3" style={props.style}>
-            <h4 className="m-2">{props.status.name} &nbsp; | &nbsp;
-             {props.status.lineStatuses[0].statusSeverityDescription}
-            </h4>
-        </div >
-    )
+    if (props.status.lineStatuses[0].statusSeverityDescription.length < 20) {
+        return (
+            <div className="card m-3" style={props.style}>
+                <h4 className="m-2">{props.status.name}
+                    <span style={{ float: "right" }}>{props.status.lineStatuses[0].statusSeverityDescription}</span>
+                </h4>
+            </div >
+        )
+    }
+    else {
+        return (
+            <div className="card m-3" style={props.style}>
+                <h4 className="m-2">{props.status.name}
+                    <span style={{ float: "right" }}>Disruption</span>
+                </h4>
+                <p>{props.status.lineStatuses[0].statusSeverityDescription}</p>
+            </div >
+        )
+    }
 }
 
 export default StatusBlock;
