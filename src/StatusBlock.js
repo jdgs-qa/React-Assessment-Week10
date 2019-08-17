29fe10003017e0393fc01723b5f2919505b2c2e3
Jonny Squire <jonnysquire@Jonnys-MacBook-Air.local>
2019-08-17 08:47:21 +0100

    Split Good Status from others

--- src/StatusBlock.js
+++ src/StatusBlock.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function StatusBlock(props) {
-    if (props.status.lineStatuses[0].statusSeverityDescription.length < 20) {
+    if (props.status.lineStatuses[0].statusSeverityDescription === "Good Service") {
+        const style = { float: "right", color: "#00ff00", backgroundColor: "#000000" }
         return (
             <div className="card m-3" style={props.style}>
-                <h4 className="m-2">{props.status.name}
-                    <span style={{ float: "right" }}>{props.status.lineStatuses[0].statusSeverityDescription}</span>
+                <h4 className="m-2"> <b>{props.status.name}</b>
+                    <span style={style}> {props.status.lineStatuses[0].statusSeverityDescription} &nbsp; ○</span>
                 </h4>
             </div >
         )
