e203533687e6268379e0871fa3b35863c5b85aaf
Jonny Squire <jonnysquire@Jonnys-MacBook-Air.local>
2019-08-19 09:59:46 +0100

    Remove lines with good service

--- src/StatusBlock.js
+++ src/StatusBlock.js
@@ -2,22 +2,35 @@ import React from 'react';
 
 function StatusBlock(props) {
     if (props.status.lineStatuses[0].statusSeverityDescription === "Good Service") {
-        const style = { float: "right", color: "#00ff00", backgroundColor: "#000000" }
+        // const style = { float: "right" }
+        return (false
+            // < div className="card m-3" style={props.style} >
+            //     <h4 className="m-2"> <b>{props.status.name}</b>
+            //         <span style={style}> {props.status.lineStatuses[0].statusSeverityDescription} &nbsp; <strong style={{ color: "#00ff00" }}>○</strong></span>
+            //     </h4>
+            // </div >
+        )
+    } else if (props.status.lineStatuses[0].statusSeverityDescription === "Part Closure") {
+        let n = props.status.lineStatuses[0].reason.indexOf(":") + 1;
+        const style = { float: "right" }
         return (
             <div className="card m-3" style={props.style}>
                 <h4 className="m-2"> <b>{props.status.name}</b>
-                    <span style={style}> {props.status.lineStatuses[0].statusSeverityDescription} &nbsp; ○</span>
+                    <span style={style}> {props.status.lineStatuses[0].statusSeverityDescription} &nbsp; <strong style={{ color: "#ffff00" }}>○</strong></span>
                 </h4>
+                <p className="m-2">{props.status.lineStatuses[0].reason.substring(n)}</p>
             </div >
         )
     }
     else {
+        let n = props.status.lineStatuses[0].reason.indexOf(":") + 1;
+        const style = { float: "right" }
         return (
             <div className="card m-3" style={props.style}>
                 <h4 className="m-2">{props.status.name}
-                    <span style={{ float: "right" }}>Disruption</span>
+                    <span style={style}> {props.status.lineStatuses[0].statusSeverityDescription} &nbsp; <strong style={{ color: "#ff0000" }}>○</strong></span>
                 </h4>
-                <p>{props.status.lineStatuses[0].statusSeverityDescription}</p>
+                <p className="m-2">{props.status.lineStatuses[0].reason.substring(n)}</p>
             </div >
         )
     }
--- src/StatusBlockList.js
+++ src/StatusBlockList.js
@@ -6,6 +6,7 @@ export default class StatusBlockList extends Component {
     constructor() {
         super();
         this.state = {
+            tim: "",
             lineStatus: [],
             stationInfo: [],
             lineColours: {
@@ -22,18 +23,28 @@ export default class StatusBlockList extends Component {
                 piccadilly: { backgroundColor: "#003688" },
                 "tfl-rail": { backgroundColor: "#0019a8" },
                 victoria: { backgroundColor: "#0098D4" },
-                "waterloo-city": { backgroundColor: "#95CDBA" }
+                "waterloo-city": { backgroundColor: "#95CDBA" },
+                tram: { backgroundColor: "#00bd19" },
+                "emirates-air-line": { backgroundColor: "#dc241f" }
             }
         }
     }
 
 
     componentDidMount() {
-        Axios.get('https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail,/status?app_id=63f8cf74&app_key=5abfdb36699070bcaa3b91012f50c03c')
+        this.interval = setInterval(() => this.setState({
+            time: Date.now(),
+            lineStatus: this.state.lineStatus,
+            stationInfo: this.state.stationInfo,
+            lineColours: this.state.lineColours
+
+        }), 300000);
+        Axios.get('https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail,cable-car,tram/status?app_id=63f8cf74&app_key=5abfdb36699070bcaa3b91012f50c03c')
             .then(res => {
                 const LineStatus = res.data;
                 console.info(LineStatus);
                 this.setState({
+                    time: this.state.time,
                     lineStatus: LineStatus,
                     stationInfo: this.state.stationInfo,
                     lineColours: this.state.lineColours
@@ -42,24 +53,35 @@ export default class StatusBlockList extends Component {
             }).catch(error => {
                 console.warn(error);
             })
-        Axios.get('https://api.tfl.gov.uk/StopPoint/490005183E?app_id=63f8cf74&app_key=5abfdb36699070bcaa3b91012f50c03c')
-            .then(res => {
-                const StationInfo = res.data;
-                console.info(StationInfo);
-                this.setState({
-                    lineStatus: this.state.lineStatus,
-                    stationInfo: StationInfo,
-                    lineColours: this.state.lineColours
-                })
-                console.info(this.state);
-            }).catch(error => {
-                console.warn(error);
-            })
+        // Axios.get('https://api.tfl.gov.uk/StopPoint/490005183E?app_id=63f8cf74&app_key=5abfdb36699070bcaa3b91012f50c03c')
+        //     .then(res => {
+        //         const StationInfo = res.data;
+        //         console.info(StationInfo);
+        //         this.setState({
+        //             time: this.state.time,
+        //             lineStatus: this.state.lineStatus,
+        //             stationInfo: StationInfo,
+        //             lineColours: this.state.lineColours
+        //         })
+        //         console.info(this.state);
+        //     }).catch(error => {
+        //         console.warn(error);
+        //     })
     }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+
 
     render() {
         return (
-            <div className="m-4">
+            <div>
+                <div className="card m-3">
+                    <h4 className="m-2" style={{ color: "black" }}> There is a good service on all other lines<span style={{ float: "right" }}><strong style={{ color: "#00ff00" }}>○</strong></span>
+                    </h4>
+                </div>
                 {this.state.lineStatus.map((lineStatus) =>
                     <StatusBlock status={lineStatus} key={lineStatus.id} style={this.state.lineColours[lineStatus.id]} />
                 )}
